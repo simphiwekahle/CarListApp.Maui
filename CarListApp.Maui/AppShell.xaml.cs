5744af446455b2cc0d57843cfad2493f1a6b8943
Sphephelo <sphephelon@hollywoodbets.net>
2024-08-22 10:53:40 +0200

    Add project files.

---
+++ CarListApp.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarListApp.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarListApp.Maui/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string Vin { get; set; } = string.Empty;
+}
